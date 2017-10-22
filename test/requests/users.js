730533c060955f59b89be6b4986b8cce94250785
Kennedy S. Dias <kennedysouzadias@gmail.com>
2017-10-22 14:26:50 -0200

    Checkpoint nos testes - tirar dúvida com o orientador

--- test/requests/users.js
+++ test/requests/users.js
@@ -1,8 +1,19 @@
-const HTTP = require('http')
+const requestfy = require('requestify')
+const os = require('os-utils')
 
+function medirHardware(label) {
+    console.log(label)
+    os.cpuUsage(function(v){
+        console.log( 'CPU usada:', v * 100, "%" );
+    });
+    console.log( 'Memória usada:', (os.totalmem() - os.freemem()) * 100 / os.totalmem(), " %" );
+
+}
 describe('Routes Users', () => {
 
-    const TIMES = 10
+    console.time("Testes")
+
+    const TIMES = 100
     const URL = 'http://localhost:3000'
     const USERS = app.dataSource.models.Users
 
@@ -14,185 +25,79 @@ describe('Routes Users', () => {
     before(() => USERS
         .remove({}))
 
+    // POST
+    medirHardware("POST")
     for (i = 0 ; i < TIMES ; i++) {
 
-        const user = {
+        let user = {
             name: `${defaultUser.name} ${i}`,
             email: `${i}${defaultUser.email}`
         }
-        const email = `${i}${defaultUser.email}`
 
-        describe('Routes /users/', () =>
+        // describe('Routes /users/', () =>
             it('POST - Create a new user', done => {
-                request
-                    .post(`/users`)
-                    .send(user)
-                    .end((err, res) => {
-                        if (err) done(err)
-                        else done()
+                requestfy.post(`${URL}/users`, user)
+                    .then( res => {
+                        done()
                     })
             })
-        )
+        // )
+    }
 
-        describe('Routes /users/:email', () => {
+    // GET
+    medirHardware("GET")
+    for (i = 0 ; i < TIMES ; i++) {
+
+        let email = `${i}${defaultUser.email}`
+
+        // describe('Routes /users/:email', () => {
             it('GET - Find a user by email', done => {
-                HTTP.get(`${URL}/users/${email}`, res =>
-                    res.on('data', chunk =>
+                requestfy.get(`${URL}/users/${email}`)
+                    .then( res => {
                         done()
-                    )
-                ).on("error", e => done(e));
+                    });
             })
+        // })
 
+    }
+
+    // PUT
+    medirHardware("PUT")
+    for (i = 0 ; i < TIMES ; i++) {
+
+        let email = `${i}${defaultUser.email}`
+
+        // describe('Routes /users/:email', () => {
             it('PUT - Update a user by email', done => {
-
                 let userUpdated = {
                     name: 'Name updated'
                 }
 
-                
-                request
-                    .put(`/users/:${email}`)
-                    .send(userUpdated)
-                    .end((err, res) => {
-                        if (err) done(err)
-                        else done()
+                requestfy.put(`${URL}/users/${email}`, userUpdated)
+                    .then( res => {
+                        done()
                     })
             })
-
-            it('DELETE - Remove a user by email', done => {
-                request
-                    .delete(`/users/:${email}`)
-                    .end((err, res) => {
-                        if (err) done(err)
-                        else done()
-                    })
-            })
-        })
+        // })
 
     }
 
-    /*
+    // DELETE
+    medirHardware("DELETE")
+    for (i = 0 ; i < TIMES ; i++) {
 
+        let email = `${i}${defaultUser.email}`
 
-    describe('Route GET /users', () => {
-        it('Retorna uma lista de USERS', (done) => {
-            request
-                .get('/users')
-                .end((err, res) => {
-                    expect(res.body[0].name)
-                        .to.be.eql(defaultUser.name);
-                    expect(res.body[0].email)
-                        .to.be.eql(defaultUser.email);
-                    expect(res.body[0].password)
-                        .to.be.eql(defaultUser.password);
-                    // expect(res.body[0].birthday)
-                    // .to.be.eql(defaultUser.birthday);
-                    expect(res.body[0].address[0].id)
-                        .to.be.eql(defaultUser.address[0].id);
-                    expect(res.body[0].address[0].street)
-                        .to.be.eql(defaultUser.address[0].street);
-                    expect(res.body[0].address[0].number)
-                        .to.be.eql(defaultUser.address[0].number);
-                    expect(res.body[0].address[0].district)
-                        .to.be.eql(defaultUser.address[0].district);
-                    expect(res.body[0].address[0].zipcode)
-                        .to.be.eql(defaultUser.address[0].zipcode);
-                    expect(res.body[0].address[0].city)
-                        .to.be.eql(defaultUser.address[0].city);
-                    expect(res.body[0].address[0].state)
-                        .to.be.eql(defaultUser.address[0].state);
-                    expect(res.body[0].address[0].city)
-                        .to.be.eql(defaultUser.address[0].city);
-                    expect(res.body[0].devices[0].type)
-                        .to.be.eql(defaultUser.devices[0].type);
-                    expect(res.body[0].devices[0].token)
-                        .to.be.eql(defaultUser.devices[0].token);
-                    // expect(res.body[0].devices[0].lastAcess)
-                    // .to.be.eql(defaultUser.devices[0].lastAcess);
-                    // expect(res.body[0].favoriros[0])
-                    // .to.be.eql(defaultUser.favoriros[0]);
-                    expect(res.body[0].active)
-                        .to.be.eql(defaultUser.active);
+        // describe('Routes /users/:email', () => {
+            it('DELETE - Remove a user by email', done => {
+                requestfy.delete(`${URL}/users/${email}`)
+                    .then( res => {
+                        done()
+                    })
+            })
+        // })
 
-                    done(err);
-                });
-        });
-    });
+    }
 
-    describe('Route GET /users/{email}', () => {
-        it('Retorna um User', (done) => {
-            request
-                .get('/users/email@default.com.br')
-                .end((err, res) => {
-                    expect(res.body.name)
-                        .to.be.eql(defaultUser.name);
-                    expect(res.body.email)
-                        .to.be.eql(defaultUser.email);
-                    expect(res.body.password)
-                        .to.be.eql(defaultUser.password);
-                    // expect(res.body.birthday)
-                    // .to.be.eql(defaultUser.birthday);
-                    expect(res.body.address[0].id)
-                        .to.be.eql(defaultUser.address[0].id);
-                    expect(res.body.address[0].street)
-                        .to.be.eql(defaultUser.address[0].street);
-                    expect(res.body.address[0].number)
-                        .to.be.eql(defaultUser.address[0].number);
-                    expect(res.body.address[0].district)
-                        .to.be.eql(defaultUser.address[0].district);
-                    expect(res.body.address[0].zipcode)
-                        .to.be.eql(defaultUser.address[0].zipcode);
-                    expect(res.body.address[0].city)
-                        .to.be.eql(defaultUser.address[0].city);
-                    expect(res.body.address[0].state)
-                        .to.be.eql(defaultUser.address[0].state);
-                    expect(res.body.address[0].city)
-                        .to.be.eql(defaultUser.address[0].city);
-                    expect(res.body.devices[0].type)
-                        .to.be.eql(defaultUser.devices[0].type);
-                    expect(res.body.devices[0].token)
-                        .to.be.eql(defaultUser.devices[0].token);
-                    // expect(res.body.devices[0].lastAcess)
-                    // .to.be.eql(defaultUser.devices[0].lastAcess);
-                    // expect(res.body.favoriros[0])
-                    // .to.be.eql(defaultUser.favoriros[0]);
-                    expect(res.body.active)
-                        .to.be.eql(defaultUser.active);
-
-                    done(err);
-                });
-        });
-    });
-
-
-    describe('Route PUT /users/{email}', () => {
-        it('Atualiza um User', (done) => {
-            const updateUser = {
-                name: 'Nome update',
-            };
-
-            request
-                .put('/users/email@default.com.br')
-                .send(updateUser)
-                .end((err, res) => {
-                    expect(res.body).to.be.eql({ ok: 1, nModified: 1, n: 1 });
-
-                    done(err);
-                });
-        });
-    });
-
-    describe('Route DELETE /users/{email}', () => {
-        it('Remove um User (Faz o atributo ATIVO = false)', (done) => {
-            request
-                .delete('/users/email@default.com.br')
-                .end((err, res) => {
-                    expect(res.statusCode).to.be.eql(204);
-
-                    done(err);
-                });
-        });
-    });
-
-    */
+    console.timeEnd("Testes")
 });
